60ee6bb727b621c9f5c893bb069dd80c792c1c9f
Pierre Mouton <pierre.mouton88@gmail.com>
2026-04-08 14:12:07 +0200

    fix: downgrade prisma to 6.19.3 to use binary engine instead of WASM

--- BACK/src/prisma/prisma.service.ts
+++ BACK/src/prisma/prisma.service.ts
@@ -1,19 +1,11 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { createPool } from 'mariadb';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    const pool = createPool({ uri: process.env.DATABASE_URL });
-    const adapter = new PrismaMariaDb(pool);
-    super({ adapter });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
